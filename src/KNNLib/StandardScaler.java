995940f508dddf8f10cb7489e2d23698271603ca
Ricky <rickyyu030102@gmail.com>
2023-03-21 16:27:24 -0700

    Adjusted StandardScaler to match Matrix implementation

--- src/KNNLib/StandardScaler.java
+++ src/KNNLib/StandardScaler.java
@@ -16,29 +16,30 @@ public class StandardScaler implements Scaler {
 
     // returns the matrix with each element scaled normally (also stores the new matrix in scaledMatrix)
     public Matrix fitTransform(Matrix data, ArrayList<Integer> fitAttributes) {
-        for (int i = 0; i < data.getMatrix().size(); i++) {
-            if (!fitAttributes.contains(i)) {
-                scaledMatrix.add(data.getMatrix().get(i));
-            } else {
-                ArrayList<Integer> row = data.getMatrix().get(i);
-                double mean = 0;
-                double stdDev = 0;
-                for (int j = 0; j < row.size(); j++) {
-                    mean += row.get(j);
-                }
-                mean /= row.size();
-                for (int j = 0; j < row.size(); j++) {
-                    stdDev += Math.pow(row.get(j) - mean, 2);
-                }
-                stdDev = Math.sqrt(stdDev / row.size());
-                means.add(mean);
-                stdDevs.add(stdDev);
-                ArrayList<Integer> scaledRow = new ArrayList<>();
-                for (int j = 0; j < row.size(); j++) {
-                    scaledRow.add((int) ((row.get(j) - mean) / stdDev));
-                }
-                scaledMatrix.add(scaledRow);
+        for (int i : fitAttributes) {
+            double mean = 0;
+            double stdDev = 0;
+            for (ArrayList<Integer> row : data.getMatrix()) {
+                mean += row.get(i);
             }
+            mean /= data.getMatrix().size();
+            for (ArrayList<Integer> row : data.getMatrix()) {
+                stdDev += Math.pow(row.get(i) - mean, 2);
+            }
+            stdDev = Math.sqrt(stdDev / data.getMatrix().size());
+            means.add(mean);
+            stdDevs.add(stdDev);
+        }
+        for (ArrayList<Integer> row : data.getMatrix()) {
+            ArrayList<Integer> scaledRow = new ArrayList<>();
+            for (int i = 0; i < row.size(); i++) {
+                if (fitAttributes.contains(i)) {
+                    scaledRow.add((int) ((row.get(i) - means.get(fitAttributes.indexOf(i))) / stdDevs.get(fitAttributes.indexOf(i))));
+                } else {
+                    scaledRow.add(row.get(i));
+                }
+            }
+            scaledMatrix.add(scaledRow);
         }
         return new Matrix(scaledMatrix);
     }
